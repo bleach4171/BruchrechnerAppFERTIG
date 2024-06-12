372882734128d4ae55688da40710f4b611e1a4e3
bleach4171 <167485971+bleach4171@users.noreply.github.com>
2024-06-13 01:07:59 +0200

    Updated on 13.06 at 1am

--- BruchrechnerApp/BruchrechnerApp/Form1.Designer.cs
+++ BruchrechnerApp/BruchrechnerApp/Form1.Designer.cs
@@ -46,148 +46,193 @@
             // 
             // textBoxZaehler1
             // 
+            this.textBoxZaehler1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(33)))), ((int)(((byte)(32)))));
             this.textBoxZaehler1.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBoxZaehler1.Location = new System.Drawing.Point(93, 93);
+            this.textBoxZaehler1.ForeColor = System.Drawing.Color.White;
+            this.textBoxZaehler1.Location = new System.Drawing.Point(62, 60);
+            this.textBoxZaehler1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBoxZaehler1.Multiline = true;
             this.textBoxZaehler1.Name = "textBoxZaehler1";
-            this.textBoxZaehler1.Size = new System.Drawing.Size(126, 53);
+            this.textBoxZaehler1.Size = new System.Drawing.Size(85, 36);
             this.textBoxZaehler1.TabIndex = 0;
             this.textBoxZaehler1.TextChanged += new System.EventHandler(this.textBoxZaehler1_TextChanged);
             // 
             // textBoxNenner1
             // 
+            this.textBoxNenner1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(33)))), ((int)(((byte)(32)))));
             this.textBoxNenner1.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBoxNenner1.Location = new System.Drawing.Point(93, 246);
+            this.textBoxNenner1.ForeColor = System.Drawing.Color.White;
+            this.textBoxNenner1.Location = new System.Drawing.Point(62, 160);
+            this.textBoxNenner1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBoxNenner1.Multiline = true;
             this.textBoxNenner1.Name = "textBoxNenner1";
-            this.textBoxNenner1.Size = new System.Drawing.Size(126, 53);
+            this.textBoxNenner1.Size = new System.Drawing.Size(85, 36);
             this.textBoxNenner1.TabIndex = 1;
             this.textBoxNenner1.TextChanged += new System.EventHandler(this.textBoxNenner1_TextChanged);
             // 
             // textBoxZaehler2
             // 
+            this.textBoxZaehler2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(33)))), ((int)(((byte)(32)))));
             this.textBoxZaehler2.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBoxZaehler2.Location = new System.Drawing.Point(474, 93);
+            this.textBoxZaehler2.ForeColor = System.Drawing.Color.White;
+            this.textBoxZaehler2.Location = new System.Drawing.Point(316, 60);
+            this.textBoxZaehler2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBoxZaehler2.Multiline = true;
             this.textBoxZaehler2.Name = "textBoxZaehler2";
-            this.textBoxZaehler2.Size = new System.Drawing.Size(126, 53);
+            this.textBoxZaehler2.Size = new System.Drawing.Size(85, 36);
             this.textBoxZaehler2.TabIndex = 2;
             // 
             // textBoxNenner2
             // 
+            this.textBoxNenner2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(33)))), ((int)(((byte)(32)))));
             this.textBoxNenner2.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBoxNenner2.Location = new System.Drawing.Point(474, 246);
+            this.textBoxNenner2.ForeColor = System.Drawing.Color.White;
+            this.textBoxNenner2.Location = new System.Drawing.Point(316, 160);
+            this.textBoxNenner2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBoxNenner2.Multiline = true;
             this.textBoxNenner2.Name = "textBoxNenner2";
-            this.textBoxNenner2.Size = new System.Drawing.Size(126, 53);
+            this.textBoxNenner2.Size = new System.Drawing.Size(85, 36);
             this.textBoxNenner2.TabIndex = 3;
             // 
             // textBoxErgebnisZaehler
             // 
-            this.textBoxErgebnisZaehler.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.textBoxErgebnisZaehler.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(33)))), ((int)(((byte)(32)))));
             this.textBoxErgebnisZaehler.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBoxErgebnisZaehler.Location = new System.Drawing.Point(733, 93);
+            this.textBoxErgebnisZaehler.ForeColor = System.Drawing.Color.White;
+            this.textBoxErgebnisZaehler.Location = new System.Drawing.Point(489, 60);
+            this.textBoxErgebnisZaehler.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBoxErgebnisZaehler.Multiline = true;
             this.textBoxErgebnisZaehler.Name = "textBoxErgebnisZaehler";
             this.textBoxErgebnisZaehler.ReadOnly = true;
-            this.textBoxErgebnisZaehler.Size = new System.Drawing.Size(169, 53);
+            this.textBoxErgebnisZaehler.Size = new System.Drawing.Size(114, 36);
             this.textBoxErgebnisZaehler.TabIndex = 4;
             this.textBoxErgebnisZaehler.TextChanged += new System.EventHandler(this.textBoxErgebnisZaehler_TextChanged);
             // 
             // textBoxErgebnisNenner
             // 
-            this.textBoxErgebnisNenner.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.textBoxErgebnisNenner.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(33)))), ((int)(((byte)(32)))));
             this.textBoxErgebnisNenner.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBoxErgebnisNenner.Location = new System.Drawing.Point(733, 246);
+            this.textBoxErgebnisNenner.ForeColor = System.Drawing.Color.White;
+            this.textBoxErgebnisNenner.Location = new System.Drawing.Point(489, 160);
+            this.textBoxErgebnisNenner.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBoxErgebnisNenner.Multiline = true;
             this.textBoxErgebnisNenner.Name = "textBoxErgebnisNenner";
             this.textBoxErgebnisNenner.ReadOnly = true;
-            this.textBoxErgebnisNenner.Size = new System.Drawing.Size(169, 53);
+            this.textBoxErgebnisNenner.Size = new System.Drawing.Size(114, 36);
             this.textBoxErgebnisNenner.TabIndex = 5;
             this.textBoxErgebnisNenner.TextChanged += new System.EventHandler(this.textBoxErgebnisNenner_TextChanged);
             // 
             // button1
             // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(33)))), ((int)(((byte)(32)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.button1.Font = new System.Drawing.Font("Century Gothic", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(273, 106);
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(182, 69);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(51, 40);
+            this.button1.Size = new System.Drawing.Size(34, 26);
             this.button1.TabIndex = 7;
             this.button1.Text = "+";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(33)))), ((int)(((byte)(32)))));
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.button2.Font = new System.Drawing.Font("Century Gothic", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(273, 175);
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(182, 114);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(51, 40);
+            this.button2.Size = new System.Drawing.Size(34, 26);
             this.button2.TabIndex = 8;
             this.button2.Text = "x";
-            this.button2.UseVisualStyleBackColor = true;
+            this.button2.UseVisualStyleBackColor = false;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
+            this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(33)))), ((int)(((byte)(32)))));
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.button3.Font = new System.Drawing.Font("Century Gothic", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button3.Location = new System.Drawing.Point(273, 239);
+            this.button3.ForeColor = System.Drawing.Color.White;
+            this.button3.Location = new System.Drawing.Point(182, 155);
+            this.button3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(51, 40);
+            this.button3.Size = new System.Drawing.Size(34, 26);
             this.button3.TabIndex = 9;
             this.button3.Text = "^";
-            this.button3.UseVisualStyleBackColor = true;
+            this.button3.UseVisualStyleBackColor = false;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button4
             // 
+            this.button4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(33)))), ((int)(((byte)(32)))));
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.button4.Font = new System.Drawing.Font("Century Gothic", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button4.Location = new System.Drawing.Point(354, 106);
+            this.button4.ForeColor = System.Drawing.Color.White;
+            this.button4.Location = new System.Drawing.Point(236, 69);
+            this.button4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(51, 40);
+            this.button4.Size = new System.Drawing.Size(34, 26);
             this.button4.TabIndex = 10;
             this.button4.Text = "-";
-            this.button4.UseVisualStyleBackColor = true;
+            this.button4.UseVisualStyleBackColor = false;
             this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // button5
             // 
+            this.button5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(33)))), ((int)(((byte)(32)))));
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.button5.Font = new System.Drawing.Font("Century Gothic", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button5.Location = new System.Drawing.Point(354, 175);
+            this.button5.ForeColor = System.Drawing.Color.White;
+            this.button5.Location = new System.Drawing.Point(236, 114);
+            this.button5.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(51, 40);
+            this.button5.Size = new System.Drawing.Size(34, 26);
             this.button5.TabIndex = 11;
             this.button5.Text = "/";
-            this.button5.UseVisualStyleBackColor = true;
+            this.button5.UseVisualStyleBackColor = false;
             this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
             // button6
             // 
+            this.button6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(33)))), ((int)(((byte)(32)))));
+            this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.button6.Font = new System.Drawing.Font("Century Gothic", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button6.Location = new System.Drawing.Point(354, 239);
+            this.button6.ForeColor = System.Drawing.Color.White;
+            this.button6.Location = new System.Drawing.Point(236, 155);
+            this.button6.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(51, 40);
+            this.button6.Size = new System.Drawing.Size(34, 26);
             this.button6.TabIndex = 12;
             this.button6.Text = "√";
-            this.button6.UseVisualStyleBackColor = true;
+            this.button6.UseVisualStyleBackColor = false;
             this.button6.Click += new System.EventHandler(this.button6_Click);
             // 
             // buttonBerechnen
             // 
+            this.buttonBerechnen.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(33)))), ((int)(((byte)(32)))));
+            this.buttonBerechnen.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.buttonBerechnen.Font = new System.Drawing.Font("Century Gothic", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.buttonBerechnen.Location = new System.Drawing.Point(642, 175);
+            this.buttonBerechnen.ForeColor = System.Drawing.Color.White;
+            this.buttonBerechnen.Location = new System.Drawing.Point(428, 114);
+            this.buttonBerechnen.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.buttonBerechnen.Name = "buttonBerechnen";
-            this.buttonBerechnen.Size = new System.Drawing.Size(49, 40);
+            this.buttonBerechnen.Size = new System.Drawing.Size(33, 26);
             this.buttonBerechnen.TabIndex = 13;
             this.buttonBerechnen.Text = "=";
-            this.buttonBerechnen.UseVisualStyleBackColor = true;
+            this.buttonBerechnen.UseVisualStyleBackColor = false;
             this.buttonBerechnen.Click += new System.EventHandler(this.buttonBerechnen_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(986, 392);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(22)))), ((int)(((byte)(23)))), ((int)(((byte)(22)))));
+            this.ClientSize = new System.Drawing.Size(657, 255);
             this.Controls.Add(this.buttonBerechnen);
             this.Controls.Add(this.button6);
             this.Controls.Add(this.button5);
@@ -202,6 +247,7 @@
             this.Controls.Add(this.textBoxNenner1);
             this.Controls.Add(this.textBoxZaehler1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Bruchrechner App";
             this.Load += new System.EventHandler(this.Form1_Load);
